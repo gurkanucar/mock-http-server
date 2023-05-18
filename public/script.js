1130f3a28f313459d72ffdf9461c9bd90aed656b
Gurkan UCAR <ucar.gurkan@hotmail.com>
2023-05-18 22:36:13 +0300

    code refactor

--- public/script.js
+++ public/script.js
@@ -1,74 +1,50 @@
-document.addEventListener("DOMContentLoaded", function () {
-  const routesTableBody = document.getElementById("routesTableBody");
-  const addRouteForm = document.getElementById("addRouteForm");
+const addRouteForm = document.getElementById("addRouteForm");
 
-  // Add Route Form Submission
-  addRouteForm.addEventListener("submit", function (event) {
-    event.preventDefault();
+addRouteForm.addEventListener("submit", function (event) {
+  event.preventDefault();
+  const newRoute = getFormData();
+  addRoute(newRoute);
+  fetchRoutes();
+  addRouteForm.reset();
+});
 
-    const routeName = document.getElementById("routeName").value;
-    const httpMethod = document.getElementById("httpMethod").value;
-    const routePath = document.getElementById("routePath").value;
-    const responseType = document.getElementById("responseType").value;
-    const apiType = document.getElementById("apiType").value;
-    const responseData = document.getElementById("response").value;
+const getFormData = () => {
+  const routeName = document.getElementById("routeName").value;
+  const httpMethod = document.getElementById("httpMethod").value;
+  const routePath = document.getElementById("routePath").value;
+  const responseType = document.getElementById("responseType").value;
+  const apiType = document.getElementById("apiType").value;
+  const responseData = document.getElementById("response").value;
 
-    const newRoute = {
-      routeName,
-      httpMethod,
-      routePath,
-      responseType,
-      apiType,
-      responseData,
-    };
+  return {
+    routeName,
+    httpMethod,
+    routePath,
+    responseType,
+    apiType,
+    responseData,
+  };
+};
 
-    fetch("/routes", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(newRoute),
-    })
-      .then((response) => {
-        if (response.ok) {
-          return response.json();
-        }
+const addRoute = (newRoute) => {
+  fetch("/routes", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(newRoute),
+  })
+    .then((response) => {
+      if (!response.ok) {
         throw new Error("Error adding route.");
-      })
-      .then((routes) => {
-        renderRoutesTable(routes);
-      })
-      .catch((error) => {
-        console.error(error);
-      });
+      }
+    })
+    .catch((error) => {
+      console.error(error);
+    });
+};
 
-    addRouteForm.reset();
-  });
-
-  // Delete Route Button Click
-  routesTableBody.addEventListener("click", function (event) {
-    if (event.target.classList.contains("delete-button")) {
-      const routeId = event.target.getAttribute("data-route-id");
-
-      fetch(`/routes/${routeId}`, {
-        method: "DELETE",
-      })
-        .then((response) => {
-          if (response.ok) {
-            return response.json();
-          }
-          throw new Error("Error deleting route.");
-        })
-        .then((routes) => {
-          renderRoutesTable(routes);
-        })
-        .catch((error) => {
-          console.error(error);
-        });
-    }
-  });
-
-  // Fetch and render routes
+const fetchRoutes = () => {
   fetch("/routes")
     .then((response) => {
       if (response.ok) {
@@ -82,29 +58,78 @@ document.addEventListener("DOMContentLoaded", function () {
     .catch((error) => {
       console.error(error);
     });
+};
 
-  // Render routes table
-  function renderRoutesTable(routes) {
-    if (!Array.isArray(routes)) {
-      console.error("Invalid routes data.");
-      return;
-    }
-
-    routesTableBody.innerHTML = "";
-
-    routes.forEach((route) => {
-      const row = document.createElement("tr");
-      row.innerHTML = `
-          <td>${route.routeName}</td>
-          <td>${route.httpMethod}</td>
-          <td>${route.routePath}</td>
-          <td>${route.responseType}</td>
-          <td>${route.apiType}</td>
-          <td>
-            <button class="delete-button" data-route-id="${route.id}">Delete</button>
-          </td>
-        `;
-      routesTableBody.appendChild(row);
-    });
+const renderRoutesTable = (routes) => {
+  if (!Array.isArray(routes)) {
+    console.error("Invalid routes data.");
+    return;
   }
+
+  routesTableBody.innerHTML = "";
+
+  routes.forEach((route) => {
+    const row = document.createElement("tr");
+    row.innerHTML = `
+        <td>${route.routeName}</td>
+        <td>${route.httpMethod}</td>
+        <td>${route.routePath}</td>
+        <td>${route.responseType}</td>
+        <td>${route.apiType}</td>
+        <td>
+          <button class="delete-button" data-route-id="${route.id}">Delete</button>
+        </td>
+      `;
+    routesTableBody.appendChild(row);
+  });
+};
+
+routesTableBody.addEventListener("click", function (event) {
+  if (event.target.classList.contains("delete-button")) {
+    const routeId = event.target.getAttribute("data-route-id");
+    deleteRoute(routeId);
+  }
+});
+
+const deleteRoute = (routeId) => {
+  fetch(`/routes/${routeId}`, {
+    method: "DELETE",
+  })
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error("Error deleting route.");
+      }
+    })
+    .then(() => {
+      fetchRoutes();
+    })
+    .catch((error) => {
+      console.error(error);
+    });
+};
+
+const populateSelectOptions = (selectElement, options) => {
+  options.forEach((option) => {
+    const optionElement = document.createElement("option");
+    optionElement.value = option;
+    optionElement.textContent = option;
+    selectElement.appendChild(optionElement);
+  });
+};
+
+document.addEventListener("DOMContentLoaded", () => {
+  const httpMethodSelect = document.getElementById("httpMethod");
+  const responseTypeSelect = document.getElementById("responseType");
+  const apiTypeSelect = document.getElementById("apiType");
+
+  const httpMethodOptions = ["GET", "POST", "PUT", "PATCH", "DELETE"];
+  populateSelectOptions(httpMethodSelect, httpMethodOptions);
+
+  const responseTypeOptions = ["RETURN_ERROR", "RANDOM_ERROR", "SUCCESS"];
+  populateSelectOptions(responseTypeSelect, responseTypeOptions);
+
+  const apiTypeOptions = ["REST", "SOAP"];
+  populateSelectOptions(apiTypeSelect, apiTypeOptions);
+
+  fetchRoutes();
 });
